d5dc7d1d44e94cc6886ec5873a17706b48358068
woojin.cheon <woojin.cheon@formalworks.com>
2021-10-22 16:25:00 +0900

    [feature] Login activity 추가

---
+++ app/src/main/java/com/clonecoding/clonetinder/ui/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.clonecoding.clonetinder.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.clonecoding.clonetinder.R
+
+/**
+ * 로그인 액티비티
+ */
+class LoginActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
