f8d524fe398d486b0607a87df918193825ac650e
woojin.cheon <woojin.cheon@formalworks.com>
2021-10-22 17:38:47 +0900

    [feature] 이메일 로그인, 회원 가입 기능 구현 - 액션바 제거 - 데이터 바인딩 추가

--- app/src/main/java/com/clonecoding/clonetinder/MainActivity.kt
+++ app/src/main/java/com/clonecoding/clonetinder/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.clonecoding.clonetinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clonecoding.clonetinder.ui.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (this.auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+        }
     }
 }
--- app/src/main/java/com/clonecoding/clonetinder/ui/LoginActivity.kt
+++ app/src/main/java/com/clonecoding/clonetinder/ui/LoginActivity.kt
@@ -1,16 +1,100 @@
 package com.clonecoding.clonetinder.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
 import com.clonecoding.clonetinder.R
+import com.clonecoding.clonetinder.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 /**
  * 로그인 액티비티
  */
 class LoginActivity : AppCompatActivity() {
-    
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        this.binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_login
+        )
+        binding.lifecycleOwner = this
+
+        this.auth = FirebaseAuth.getInstance()
+
+        this.initLoginButton()
+        this.initSignUpButton()
+        this.initEmailAndPasswordEditText()
+    }
+
+    private fun initEmailAndPasswordEditText() {
+
+        this.binding.emailEditText.addTextChangedListener {
+            val enable = this.binding.emailEditText.text.isNotEmpty() &&
+                    this.binding.passwordEditText.text.isNotEmpty()
+            this.binding.loginButton.isEnabled = enable
+            this.binding.signUpButton.isEnabled = enable
+        }
+        this.binding.passwordEditText.addTextChangedListener {
+            val enable = this.binding.emailEditText.text.isNotEmpty() &&
+                    this.binding.passwordEditText.text.isNotEmpty()
+            this.binding.loginButton.isEnabled = enable
+            this.binding.signUpButton.isEnabled = enable
+        }
+    }
+
+    private fun initLoginButton() {
+
+        val loginButton = this.binding.loginButton
+        loginButton.setOnClickListener {
+
+            val emailEditText = this.binding.emailEditText
+            val passwordEditText = this.binding.passwordEditText
+
+            this.auth.signInWithEmailAndPassword(
+                emailEditText.text.toString(),
+                passwordEditText.text.toString()
+            ).addOnCompleteListener(this) {
+
+                if (it.isSuccessful) {
+
+                    finish()
+                } else {
+
+                    Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun initSignUpButton() {
+
+        val signUpButton = this.binding.signUpButton
+        signUpButton.setOnClickListener {
+
+            val emailEditText = this.binding.emailEditText
+            val passwordEditText = this.binding.passwordEditText
+
+            this.auth.createUserWithEmailAndPassword(
+                emailEditText.text.toString(),
+                passwordEditText.text.toString()
+            ).addOnCompleteListener(this) {
+
+                if(it.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                } else {
+
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
